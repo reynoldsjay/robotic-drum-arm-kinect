59af571e14ba05fc2f9167358c30d99b3f8deaa3
Jay Reynolds <reynoldsjay@gatech.edu>
2015-03-26 14:18:43 -0400

    converts kinect video to opencv format and displays rgb and depth map windows

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# robotic-drum-arm-kinect
+# Robotic Drumming Arm Kinect App
 Kinect app that handles sensing for a robotic drumming arm.
+
+Requires OpenNI2 and OpenCV2.
--- main.cpp
+++ main.cpp
@@ -1,9 +1,101 @@
 #include <OpenNI.h>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2\core\core.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+#include <opencv2\highgui\highgui.hpp>
+#include <iostream>
 
+using namespace openni;
 using namespace cv;
 
 int main(int argc, char** argv) {
 
-}
+	OpenNI::initialize();
+	puts("Kinect initialization...");
+	Device device;
+	if (device.open(openni::ANY_DEVICE) != 0)
+	{
+		puts("Kinect not found !");
+		return -1;
+	}
+	puts("Kinect opened");
+	VideoStream depth, color;
+	color.create(device, SENSOR_COLOR);
+	color.start();
+	puts("Camera ok");
+	depth.create(device, SENSOR_DEPTH);
+	depth.start();
+	puts("Depth sensor ok");
+	VideoMode paramvideo;
+	paramvideo.setResolution(640, 480);
+	paramvideo.setFps(30);
+	paramvideo.setPixelFormat(PIXEL_FORMAT_DEPTH_100_UM);
+	depth.setVideoMode(paramvideo);
+	paramvideo.setPixelFormat(PIXEL_FORMAT_RGB888);
+	color.setVideoMode(paramvideo);
+	puts("Set video modes.");
+
+	// If the depth/color synchronisation is not necessary, start is faster :
+	device.setDepthColorSyncEnabled(false);
+
+	// Otherwise, the streams can be synchronized with a reception in the order of our choice :
+	//device.setDepthColorSyncEnabled( true );
+	//device.setImageRegistrationMode( openni::IMAGE_REGISTRATION_DEPTH_TO_COLOR );
+
+	VideoStream** stream = new VideoStream*[2];
+	stream[0] = &depth;
+	stream[1] = &color;
+	puts("Kinect initialization completed");
+
+
+	if (device.getSensorInfo(SENSOR_DEPTH) != NULL)
+	{
+		VideoFrameRef depthFrame, colorFrame;
+		cv::Mat colorcv(cv::Size(640, 480), CV_8UC3, NULL);
+		cv::Mat depthcv(cv::Size(640, 480), CV_16UC1, NULL);
+		cv::namedWindow("RGB", CV_WINDOW_AUTOSIZE);
+		cv::namedWindow("Depth", CV_WINDOW_AUTOSIZE);
+
+		int changedIndex;
+		while (device.isValid())
+		{
+			OpenNI::waitForAnyStream(stream, 2, &changedIndex);
+			switch (changedIndex)
+			{
+			case 0:
+				depth.readFrame(&depthFrame);
+
+				if (depthFrame.isValid())
+				{
+					depthcv.data = (uchar*)depthFrame.getData();
+					cv::imshow("Depth", depthcv);
+				}
+				break;
+
+			case 1:
+				color.readFrame(&colorFrame);
+
+				if (colorFrame.isValid())
+				{
+					colorcv.data = (uchar*)colorFrame.getData();
+					cv::cvtColor(colorcv, colorcv, CV_BGR2RGB);
+					cv::imshow("RGB", colorcv);
+				}
+				break;
+
+			default:
+				puts("Error retrieving a stream");
+			}
+			cv::waitKey(1);
+		}
+
+		cv::destroyWindow("RGB");
+		cv::destroyWindow("Depth");
+	}
+	depth.stop();
+	depth.destroy();
+	color.stop();
+	color.destroy();
+	device.close();
+	OpenNI::shutdown();
+
+} 
